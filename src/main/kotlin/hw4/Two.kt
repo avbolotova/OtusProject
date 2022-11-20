1b5d9634c8fd2fdd51c292fdf57c6261f98e48d5
avbolotova <direct.pavlova@yandex.ru>
2022-11-20 17:28:54 +0400

    Добавила ветку HW-4, домашки лежат в package hw4

--- src/main/kotlin/hw4/Two.kt
+++ src/main/kotlin/hw4/Two.kt
@@ -1,11 +1,15 @@
 package hw4
 
+
 fun main() {
     val inputNumber = readln().toInt()
     println(factorial(inputNumber))
 }
 
 fun factorial (inputNumber:Int): Int {
-    if (inputNumber <= 1) return 1
-    return inputNumber*factorial(inputNumber-1)
+    if (inputNumber == 0) return 1
+    return inputNumber * factorial(inputNumber - 1)
+
+
 }
+
---
+++ src/main/kotlin/hw5/TwoHWFive.kt
@@ -0,0 +1,4 @@
+package hw5
+
+class TwoHWFive {
+}
